dab5efa17e7792a84d729826f1f7dd3ec34b8a29
msopiann <muhammadsopian452@gmail.com>
2024-08-09 00:32:51 +0700

    chore: add meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Suitmedia Assignment",
+      meta: [
+        {
+          name: "description",
+          property: "description",
+          content:
+            "This website, created by Muhammad Sopian, serve as a task assignment for internship as a Frontend Developer at Suitmedia.",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+    },
+  },
+});
